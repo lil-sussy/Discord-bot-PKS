68ca7b885d0eb6b746d78a54b84954667eea1720
lil-sussy <naykeysnet@gmail.com>
2024-04-12 15:05:50 +0200

    firebase non-integration xd

--- slash/confession.js
+++ slash/confession.js
@@ -1,13 +1,18 @@
 const { MessageEmbed } = require("discord.js");
 
+const firebase_admin = require("firebase-admin");
+const serviceAccount = require("path/to/your/service-account-file.json");
+
+// firebase_admin.initializeApp({
+// 	credential: firebase_admin.credential.cert(serviceAccount),
+// });
+
+// const db = admin.firestore();
+
 exports.run = async (client, interaction) => {
-  // Retrieve the confession text from the interaction
   const confession = interaction.options.getString("confession");
-
-  // ID of the dedicated anonymous confession channel
+  const userTag = interaction.user.tag; // Storing the user's Discord tag, ensure user privacy.
   const confessionChannelId = "1227954337603653652";
-
-  // Fetch the channel from the client's channels cache
   const confessionChannel = client.channels.cache.get(confessionChannelId);
 
   // Make sure the channel exists
@@ -23,21 +28,39 @@ exports.run = async (client, interaction) => {
     .setColor("#cc00f5")
     .setFooter("55644546545");
 
+
+  // Create a new confession document in the 'confessions' collection
+  const confessionDoc = db.collection("confessions").doc();
+
+  confessionChannel
+		.send({ embeds: [embed] })
+		.then((message) => {
+			// Message sent successfully, now save the message ID to Firestore
+			const confessionDoc = db.collection("confessions").doc();
+
+			// return confessionDoc.set({
+			// 	confessionId: confessionDoc.id, // Firestore auto-generates a unique ID for the document
+			// 	messageId: message.id, // Save the Discord message ID
+			// 	confession: confession,
+			// 	user: "ANONYME XD", // Consider hashing this or leaving it out for anonymity.
+			// });
+		})
+		.then(() => {
+			// Firestore document updated, confirm to the user
+			interaction.reply({ content: "Ta confesswion a bwien éwé postwée /ᐠ≽•ヮ•≼マ !", ephemeral: true });
+		})
+		.catch((error) => {
+			// Handle any errors
+			console.error("Error sending message or writing document: ", error);
+			interaction.reply({ content: "Malaise, une ewweur s'est produitwe. /ᐠﹷ ‸ ﹷ ᐟﾉ", ephemeral: true });
+		});
+
   // Send the confession to the anonymous confession channel
-  confessionChannel.send({embeds : [embed]})
-    .then(() => {
-      // Confirm to the user that their confession has been posted (only they can see this)
-      interaction.reply({ content: "Ta confesswion a bwien éwé postwée /ᐠ≽•ヮ•≼マ !", ephemeral: true });
-    })
-    .catch(error => {
-      console.error("Error sending message: ", error);
-      interaction.reply({ content: "Malaise, une ewweur s'est produitwe. /ᐠﹷ ‸ ﹷ ᐟﾉ", ephemeral: true });
-    });
 };
 
 exports.commandData = {
 	name: "confession",
-	description: "Poste ta confession anonymement !!",
+	description: "Poste ta confesswion anonywewent ᓚᘏᗢ !!",
 	options: [
 		{
 			name: "confession",
